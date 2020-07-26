4b79ba3bef7d98526c370d7c862c378f0d2bcd50
Guilherme <guilherme_campiotto@hotmail.com>
2020-07-25 22:46:16 -0300

    Audio assets
    Signed-off-by: Guilherme <guilherme_campiotto@hotmail.com>

---
+++ Assets/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Text level;
+    int levelNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelNumber = SceneManager.GetActiveScene().buildIndex + 1;
+        
+        if (level != null)
+        {
+            level.text = level.text + levelNumber;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public static SoundController instance { get; private set; } = null;
+    string sound;
+
+    void Awake()
+    {
+        //clickSound = Resources.Load<AudioClip>("Sounds/Sound_Effects/UI/Hit2");
+
+        if (gameObject.CompareTag("SoundController") && instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else if (gameObject.CompareTag("SoundController"))
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+    private void Start()
+    {
+        sound = PlayerPrefs.GetString("Sound");
+        if (sound == "Off")
+        {
+            audioSource.mute = true;
+        }
+    }
+
+    public void PlayAudioOnce(AudioClip clip, float volume = 1f)
+    {
+        audioSource.PlayOneShot(clip, volume);
+    }
+
+    public void MuteAudio()
+    {
+        audioSource.mute = true;
+    }
+
+    public void PlayAudio()
+    {
+        audioSource.mute = false;
+    }
+
+    public void PlayWithLoop(AudioClip music)
+    {
+        audioSource.Stop();
+        audioSource.clip = music;
+        audioSource.Play();
+    }
+
+}
