2cc53d4edf331606fc12f9128f847bd96c500095
DESKTOP-1MJ8H7L\guilh <guilherme_campiotto@hotmail.com>
2020-12-19 17:22:44 -0300

    New color for background

--- Assets/Scripts/GameConfiguration.cs
+++ Assets/Scripts/GameConfiguration.cs
@@ -7,10 +7,10 @@ public static class GameConfiguration
     public static bool fixedBackground = false;
     public static List<Color> colorsList = new List<Color>() 
     {
-        new Color32(151, 240, 151, 255),
+        new Color32(53, 90, 53, 255),
         new Color32(0, 150, 88, 255),
-        new Color32(0, 102, 255, 255), // parecido
-        new Color32(74, 134, 232, 255), // parecido
+        new Color32(0, 102, 255, 255),
+        new Color32(74, 134, 232, 255),
         new Color32(171, 61, 244, 255),
         new Color32(89, 70, 164, 255)
     };
